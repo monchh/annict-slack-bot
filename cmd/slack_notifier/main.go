8029a4dfd330b0b0b7f6d764ffd9a414be2ed56f
monchh <fuzitamakotobaseball@gmail.com>
2026-05-10 03:17:22 +0900

    refactoring the norigier app

--- cmd/slack_notifier/main.go
+++ cmd/slack_notifier/main.go
@@ -5,18 +5,9 @@ import (
 	"fmt"
 	"log"
 	"log/slog"
-	"net/http"
 	"os"
-	"time"
 
-	"github.com/monchh/annict-slack-bot/infrastructure/annict"
 	"github.com/monchh/annict-slack-bot/infrastructure/config"
-	"github.com/monchh/annict-slack-bot/infrastructure/httpclient"
-	"github.com/monchh/annict-slack-bot/interfaces/presenter"
-	"github.com/monchh/annict-slack-bot/interfaces/repository"
-	"github.com/monchh/annict-slack-bot/interfaces/validator"
-	"github.com/monchh/annict-slack-bot/pkg/jst"
-	"github.com/monchh/annict-slack-bot/usecase"
 	"github.com/monchh/annict-slack-bot/usecase/annictcmd"
 	"github.com/slack-go/slack"
 )
@@ -55,46 +46,24 @@ func run(ctx context.Context, cfg *config.Config) error {
 		return fmt.Errorf("SCHEDULE_CHANNEL_ID is not set")
 	}
 
-	// 1. Dependency Injection
-	annictAuthHttpClient := &http.Client{
-		Transport: &config.AnnictAuthTransport{
-			Token: cfg.AnnictToken,
-			Transport: http.DefaultTransport,
-		},
-		Timeout:   30 * time.Second,
-	}
-	annictClient := annict.NewClient(annictAuthHttpClient, cfg.AnnictEndpoint, nil)
-	annictRepo := repository.NewAnnictRepository(annictClient, slog.Default())
-	httpValidator := validator.NewHTTPImageValidator(httpclient.NewClient(cfg.ImageCheckTimeout))
-	slackPresenter := presenter.NewSlackProgramPresenter(cfg.AnnictLimitNumToDisplay)
-	annictInfoGetter := usecase.NewAnnictInfoGetter(annictRepo, httpValidator)
+	// Prepare Slack Client
+	apiClient := slack.New(cfg.SlackHomeIotToken)
+	annictSlackApp := slack.New(cfg.SlackBotToken)
 
-	// 2. Prepare Slack Client
-	apiClient := slack.New(cfg.SlackBotToken)
-	authTest, err := apiClient.AuthTest()
+	authTest, err := annictSlackApp.AuthTest()
 	if err != nil {
 		return fmt.Errorf("slack AuthTest failed: %w", err)
 	}
 
-	// 3. Execute Business Logic
-	nowJST := jst.Now()
-	slog.Info("Executing scheduled notification", "channel", cfg.ScheduleChannelID)
-	output, err := annictInfoGetter.Execute(ctx)
-	if err != nil {
-		return fmt.Errorf("error executing usecase: %w", err)
-	}
-
 	// 4. Format and Post Message
-	return postNotification(ctx, apiClient, cfg.ScheduleChannelID, authTest.UserID, output, slackPresenter, nowJST)
+	return postNotification(ctx, apiClient, cfg.ScheduleChannelID, authTest.UserID)
 }
 
-func postNotification(ctx context.Context, api *slack.Client, channelID, botUserID string, output *usecase.AnnictInfoGetterOutput, p *presenter.SlackProgramPresenter, date time.Time) error {
-	blocks := p.FormatCombinedPrograms(output.Programs, output.LibraryEntries, date)
+func postNotification(ctx context.Context, api *slack.Client, channelID, botUserID string) error {
 	mentionText := fmt.Sprintf("<@%s> %s", botUserID, annictcmd.ANNICT_TODAY)
 
 	_, _, err := api.PostMessageContext(ctx, channelID,
 		slack.MsgOptionText(mentionText, false),
-		slack.MsgOptionBlocks(blocks...),
 	)
 	if err != nil {
 		return fmt.Errorf("failed to post message: %w", err)
